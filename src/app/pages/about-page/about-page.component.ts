1cbc8cf9e6045f28362d8ee7a77fc826ad2187fb
Jhonny <jhonnycgarcia@gmail.com>
2024-10-24 19:31:36 -0500

    SPA Tradicional

---
+++ src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './about-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class AboutPageComponent {
+
+}
