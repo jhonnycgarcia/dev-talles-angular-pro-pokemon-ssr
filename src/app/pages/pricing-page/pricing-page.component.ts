2f119499cc29c8e510dc7faecf9b7b217339ddb7
Jhonny <jhonnycgarcia@gmail.com>
2024-10-25 00:12:58 -0500

    PLATAFORM_ID - Cliente y Server

--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -12,8 +13,10 @@ export default class PricingPageComponent implements OnInit {
 
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+    // console.log({ isPlatformServer: isPlatformServer(this.platform) });
     this.title.setTitle('Precios del servicio');
     this.meta.updateTag({ name: 'description', content: 'Esta es la página de precios' });
     this.meta.updateTag({ name: 'og:title', content: 'Precios del servicio' });
