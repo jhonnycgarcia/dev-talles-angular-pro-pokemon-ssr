141d923d595d0b4973e75e7bf313475c2059c3b3
Jhonny <jhonnycgarcia@gmail.com>
2024-10-27 00:51:04 -0500

    ApplicationRef - STABLE

--- src/app/pages/pokemons-page/pokemons-page.component.ts
+++ src/app/pages/pokemons-page/pokemons-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
 import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
@@ -13,11 +13,19 @@ import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon
 export default class PokemonsPageComponent implements OnInit {
 
   // public isLoading = signal(true);
+  // private appRef = inject(ApplicationRef);
+  // private $appState = this.appRef.isStable.subscribe((isStable) => {
+  //   console.log({ isStable });
+  // })
 
   ngOnInit(): void {
     // setTimeout(() => {
     //     this.isLoading.set(false);
     // }, 5000);
   }
+
+  // ngOnDestroy(): void {
+  //   // this.$appState.unsubscribe();
+  // }
 
 }
