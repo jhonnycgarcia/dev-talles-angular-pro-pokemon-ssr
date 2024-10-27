8fac86374406c0674d03fb1771d5310c5b12c1de
Jhonny <jhonnycgarcia@gmail.com>
2024-10-27 03:27:46 -0500

    Página actual - QueryParameters

--- src/app/pages/pokemons-page/pokemons-page.component.ts
+++ src/app/pages/pokemons-page/pokemons-page.component.ts
@@ -1,8 +1,13 @@
 import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop'
+
+import { SimplePokemon } from '../../pokemons/interfaces';
+
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
 import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
-import { SimplePokemon } from '../../pokemons/interfaces';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-pokemons-page',
@@ -14,8 +19,17 @@ import { SimplePokemon } from '../../pokemons/interfaces';
 })
 export default class PokemonsPageComponent implements OnInit {
 
-  private pokemonSrv = inject(PokemonsService);
   public pokemons = signal<SimplePokemon[]>([]);
+  private pokemonSrv = inject(PokemonsService);
+  private route = inject(ActivatedRoute);
+
+  public currentPage = toSignal<number>(
+    this.route.queryParamMap.pipe(
+      map((params) => params.get('page') ?? '1' ),
+      map((page) => (isNaN(+page) ? 1 : +page) ),
+      map((page) => Math.max(1, page) )
+    )
+  );
 
   // public isLoading = signal(true);
   // private appRef = inject(ApplicationRef);
@@ -31,7 +45,9 @@ export default class PokemonsPageComponent implements OnInit {
   }
 
   public loadPokemons(nextPage: number = 0): void {
-    this.pokemonSrv.loadPage(nextPage)
+    const pageToLoad = this.currentPage()! + nextPage;
+
+    this.pokemonSrv.loadPage(pageToLoad)
       .subscribe((pokemons) => {
         this.pokemons.set(pokemons);
       });
