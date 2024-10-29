c87eb3cdcfc042bceae17d706e88a7b49799ebca
Jhonny <jhonnycgarcia@gmail.com>
2024-10-28 22:40:48 -0500

    Prueba - Cargar la primera página de Pokemons

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+
+`https://angular.dev/guide/prerendering`
+`https://angular.dev/guide/testing/services#testing-http-services`
+`https://angular.dev/guide/http/testing`
+`https://angular.dev/guide/http/testing#expecting-and-answering-requests`
--- src/app/pokemons/services/pokemons.service.spec.ts
+++ src/app/pokemons/services/pokemons.service.spec.ts
@@ -1,29 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
 
 import { PokemonsService } from './pokemons.service';
-import { SimplePokemon } from '../interfaces';
+import { PokeAPIResponse, SimplePokemon } from '../interfaces';
+
+const mockPokeResponse: PokeAPIResponse = {
+  "count": 1302,
+  "next": "https://pokeapi.co/api/v2/pokemon?offset=10&limit=10",
+  "previous": null,
+  "results": [
+      {
+          "name": "bulbasaur",
+          "url": "https://pokeapi.co/api/v2/pokemon/1/"
+      },
+      {
+          "name": "ivysaur",
+          "url": "https://pokeapi.co/api/v2/pokemon/2/"
+      }
+  ]
+};
 
 const expectedPokemons: SimplePokemon[] = [
-  { id: '1', name: 'Bulbasaur' },
-  { id: '2', name: 'Ivysaur' },
-  { id: '3', name: 'Venusaur' },
-  { id: '4', name: 'Charmander' },
-  { id: '5', name: 'Charmeleon' },
-  { id: '6', name: 'Charizard' },
-  { id: '7', name: 'Squirtle' },
-  { id: '8', name: 'Wartortle' },
-  { id: '9', name: 'Blastoise' },
+  { id: '1', name: 'bulbasaur' },
+  { id: '2', name: 'ivysaur' }
 ];
 
 const mockPokemon = {
   id: '1',
-  name: 'Bulbasaur',
+  name: 'bulbasaur',
 }
 
 describe('PokemonsService', () => {
   let service: PokemonsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -33,9 +43,28 @@ describe('PokemonsService', () => {
       ]
     });
     service = TestBed.inject(PokemonsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should load a page of SimplePokemons', async () => {
+
+    service.loadPage(1).subscribe((pokemons) => {
+      expect(pokemons).toEqual(expectedPokemons);
+    });
+
+    const req = httpMock.expectOne(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`);
+    expect(req.request.method).toBe('GET');
+
+    // Establecer el mock de respuesta
+    req.flush(mockPokeResponse);
+  });
+
 });
